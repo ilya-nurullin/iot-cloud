6f167d6290bfecea7028aa809aed7ab49423d344
Gilman M <gilman.mm@parmalogica.ru>
2020-01-13 14:19:19 +0500

    migrated to Postgres

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 #pip3 install Flask
+#pip3 install psycopg2
 
+import psycopg2
 import http.server
 import socketserver
 from threading import Thread
 import time
 import sys
 import json
-import pymysql
 
 from flask import Flask
 from flask import request
@@ -17,9 +18,9 @@ PORT = 8000
 
 scripts = {} # '1': "print(\"It works\")"
 
-con = pymysql.connect('localhost', 'root',  
-    'password', 'SmartHouse', 
-    autocommit=True)      
+con = psycopg2.connect(host='localhost', port=5432, user='postgres',  
+    password='password', dbname='SmartHouse')
+con.autocommit = True
 
 
 @app.route('/')
@@ -53,24 +54,22 @@ def addScriptToDB():
     return insertOrUpdate(name, xmlCode, pyCode,is_enabled)
         
 @app.route('/script/addToRun', methods=['POST'])
-def addScriptToRun():    
-    inputs = request.get_json()
-    
-    if ('name' not in inputs):
-        return ({'status': 'data_error', 'message': 'name expected'}, 400)
-         
-    name = inputs['name']
-    with con:
-        cur = con.cursor()
-        query = f"SELECT  pyScript FROM Scripts WHERE scriptTitle = \'{name}\'"
-        cur.execute(query)
-        script = cur.fetchone()
-        if(script == None):
-            return {'Error': 'ScriptNotFound'}
-        else:
-            print(script)
-            setScript(name, script[0])
-            return {'status': 'Added to Run'}
+def addScriptToRun():
+	inputs = request.get_json()
+	if ('name' not in inputs):
+		return ({'status': 'data_error', 'message': 'name expected'}, 400)
+	name = inputs['name']
+	with con:
+		cur = con.cursor()
+		query = f"SELECT  py_script FROM smarthouse.scripts WHERE script_title = \'{name}\'"
+		cur.execute(query)
+		script = cur.fetchone()
+		if(script == None):
+			return {'Error': 'ScriptNotFound'}
+		else:
+			print(script)
+			setScript(name, script[0])
+			return {'status': 'Added to Run'}
 
 @app.route('/script/all')
 def allScripts():
@@ -81,10 +80,10 @@ def allScripts():
 def changeScriptStatus(name, status):
 	with con:
 		cur = con.cursor()
-		query = f"UPDATE Scripts SET isEnabled = \'{status}\' where scriptTitle = \'{name}\'"
+		query = f"UPDATE smarthouse.scripts SET is_enabled = \'{status}\' where script_title = \'{name}\'"
 		cur.execute(query)
 		#cur.flush()
-		#query = f"SELECT scriptTitle, isEnabled FROM Scripts WHERE scriptTitle = \'{name}\'"
+		#query = f"SELECT script_title, is_enabled FROM smarthouse.scripts WHERE script_title = \'{name}\'"
 		#cur.execute(query)
 		#result = cur.fetchone()
 		#if(script == None):
@@ -103,15 +102,15 @@ def runner():
 def insertOrUpdate(name, xmlCode, pyCode,isEnabled):
     with con:
         cur = con.cursor()
-        query = f"SELECT * FROM Scripts WHERE scriptTitle = \"{name}\""
+        query = f"SELECT * FROM smarthouse.scripts WHERE script_title = \'{name}\'"
         cur.execute(query)
         script = cur.fetchone()
         if(script == None):
-            query = f"INSERT INTO Scripts VALUES ( 0,\'{name}\', \'{pyCode}\', \'{xmlCode}\', {isEnabled});"
+            query = f"INSERT INTO smarthouse.scripts VALUES ( 0,\'{name}\', \'{pyCode}\', \'{xmlCode}\', {isEnabled});"
             cur.execute(query)
             return("inserted")
         else:
-            query = f"UPDATE Scripts SET pyScript=\'{pyCode}\', xmlScript=\'{xmlCode}\',isEnabled= {isEnabled} where scriptTitle =\'{name}\'"
+            query = f"UPDATE smarthouse.scripts SET py_script=\'{pyCode}\', xml_script=\'{xmlCode}\',is_enabled= {isEnabled} where script_title =\'{name}\'"
             cur.execute(query)
             return("updated")
 
@@ -119,12 +118,12 @@ def insertOrUpdate(name, xmlCode, pyCode,isEnabled):
 def getScriptsByEnabled(is_enabled=None):
     whereClause = "";
     if (is_enabled == True):
-        whereClause = "WHERE isEnabled = True";
+        whereClause = "WHERE is_enabled = True";
     elif (is_enabled == False):
-        whereClause = "WHERE isEnabled = False";
+        whereClause = "WHERE is_enabled = False";
     with con:
         cur = con.cursor()
-        query = "SELECT scriptTitle, pyScript FROM Scripts "+whereClause
+        query = f"SELECT script_title, py_script FROM smarthouse.scripts {whereClause}"
         cur.execute(query)
         rows = cur.fetchall()
     return rows
